069037c5e5037992abca6b4811699f0e19c72184
Nina Mäkinen <onima019@edu.xamk.fi>
2020-06-09 21:36:17 +0300

    Coins added

---
+++ merirosvoja-mikkelissa/Assets/Code/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.GetComponent<Player>().score += 1;
+            Destroy(gameObject, 0.5f);
+
+        }
+    }
+}
