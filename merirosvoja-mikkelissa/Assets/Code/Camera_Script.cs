ba81c3ca6986be88240ba9107153ef43dad9bd92
Nina Mäkinen <onima019@edu.xamk.fi>
2020-06-09 16:41:34 +0300

    Basic attack animation and camera follow

---
+++ merirosvoja-mikkelissa/Assets/Code/Camera_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Script : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 GetPosition = this.GetComponent<Transform>().position;
+        GetPosition.x = this.player.GetComponent<Transform>().position.x;
+        GetPosition.y = this.player.GetComponent<Transform>().position.y;
+        this.GetComponent<Transform>().position = GetPosition;
+    }
+}
---
+++ merirosvoja-mikkelissa/Assets/Code/PlayerCombat.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject sword;
+    public bool attackAnimationPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) //Left mouse click
+        {
+            // Debug.Log("attack");
+            MeleeAttack();
+           
+        }
+       
+        
+    }
+    void MeleeAttack()
+    {
+        Invoke("hideSword", 3.4f);
+            attackAnimationPlaying = true;
+        
+            Debug.Log("lyö");
+            sword.SetActive(true);
+            //Play animation
+            anim.SetTrigger("Attack");
+            attackAnimationPlaying = false;
+
+           
+            Debug.Log("piilo");
+        
+        // 
+        //sword.SetActive(false);
+        //Detect enemies in range of attack
+        //Damage the enemy
+    }
+    void hideSword()
+    {
+        sword.SetActive(false);
+
+    }
+}
