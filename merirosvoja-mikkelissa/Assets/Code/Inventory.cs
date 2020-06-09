7c370ecc486988124e03e20608c9684fbcd3833d
HereticHorse <leena.karjalainen92@gmail.com>
2020-06-09 17:47:07 +0300

    inventory
    inventory

---
+++ merirosvoja-mikkelissa/Assets/Code/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool[] isFull;
+    public GameObject[] slots;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
