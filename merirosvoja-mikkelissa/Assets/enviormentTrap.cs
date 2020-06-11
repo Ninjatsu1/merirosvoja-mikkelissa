2a371e40003fb2ca1e2a3d5e4624d424827aec42
HereticHorse <leena.karjalainen92@gmail.com>
2020-06-11 18:18:48 +0300

    animatoin and props
    hyttynen, karhy ja mayra on animoitu
    niista on tehty prefabit ja samoin pelaajasta, saappaasta, aurinko voiteesta ja rikkoutuneesta pullosta

---
+++ merirosvoja-mikkelissa/Assets/enviormentTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enviormentTrap : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            Health.health -= 1;
+            Destroy(gameObject);
+
+        }
+    }
+}
