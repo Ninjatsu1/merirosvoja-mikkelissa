0c984602c1c1392b0f5c71f2bfebc40f228ac2ec
HereticHorse <leena.karjalainen92@gmail.com>
2020-06-11 09:38:54 +0300

    Väli mergeys
    teen väli mergeemisen jotta voin jatkaa toisiin koodeihin

---
+++ merirosvoja-mikkelissa/Assets/Code/BoatPatrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    private float waitTime;
+    public float startWaitTime;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+        waitTime = startWaitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+    }
+
+
+
+}
